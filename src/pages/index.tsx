d4ec6f771a359fae7f7891b34ffc4e48a4173029
hema <aussonia@gmail.com>
2024-01-22 13:11:25 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
+            Mi corazón soporta el peso abrumador de sus rigores
           </p>
           <div>
             <a
